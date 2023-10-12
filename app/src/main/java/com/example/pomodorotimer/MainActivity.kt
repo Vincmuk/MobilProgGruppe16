53b9fc7820ed5f306ae429c774577d5e898613bc
Robby <rdahlman32@gmail.com>
2023-10-12 15:29:59 +0200

    Added Timer.

--- app/src/main/java/com/example/pomodorotimer/MainActivity.kt
+++ app/src/main/java/com/example/pomodorotimer/MainActivity.kt
@@ -1,46 +1,56 @@
 package com.example.pomodorotimer
 
 import android.os.Bundle
-import androidx.activity.ComponentActivity
-import androidx.activity.compose.setContent
-import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
-import com.example.pomodorotimer.ui.theme.PomodoroTimerTheme
+import android.os.CountDownTimer
+import android.util.Log
+import android.widget.TextView
+import android.widget.Toast
+import androidx.appcompat.app.AppCompatActivity
+import timerx.Timer
+import timerx.buildTimer
+import java.util.concurrent.TimeUnit
 
-class MainActivity : ComponentActivity() {
+
+class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContent {
-            PomodoroTimerTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    Greeting("Android")
-                }
+        setContentView(R.layout.activity_main)
+
+        val text_time = findViewById<TextView>(R.id.text_time)
+        val btn_start = findViewById<TextView>(R.id.btn_start)
+        val btn_stop = findViewById<TextView>(R.id.btn_stop)
+        val btn_pause = findViewById<TextView>(R.id.btn_pause)
+        val btn_addtask = findViewById<TextView>(R.id.btn_addtask)
+
+        var cTimer: CountDownTimer? = null
+        lateinit var timer: Timer
+
+        timer = buildTimer {
+            startFormat("MM:SS:L")
+            startTime(15, TimeUnit.MINUTES)
+            useExactDelay(true)
+            onTick { millis, formattedTime ->
+                text_time.text = formattedTime
+                Log.i("Timer", "Remainingtime = $millis")
+            }
+            onFinish {
+                showToast("Finished!")
             }
         }
+
+        text_time.text = timer.formattedStartTime
+
+        btn_start.setOnClickListener { timer.start() }
+        btn_stop.setOnClickListener {
+            timer.stop()
+            timer.setTime(15, TimeUnit.MINUTES)
+            text_time.text = timer.remainingFormattedTime
+        }
+        btn_pause.setOnClickListener { timer.stop() }
+
     }
-}
 
-@Composable
-fun Greeting(name: String, modifier: Modifier = Modifier) {
-    Text(
-        text = "Hello $name!",
-        modifier = modifier
-    )
-}
-
-@Preview(showBackground = true)
-@Composable
-fun GreetingPreview() {
-    PomodoroTimerTheme {
-        Greeting("Android")
+    private fun showToast(text: String) {
+        Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
     }
 }
