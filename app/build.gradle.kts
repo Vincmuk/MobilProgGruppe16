c69d5b4e06281e087d8ba2e6d9cd8cde2f7b7fea
Ahmad  Kh <mad.fox1995.ak@gmail.com>
2023-10-13 20:34:02 +0200

    logg inn.................

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -54,15 +54,15 @@ dependencies {
 
     implementation ("com.github.arsvechkarev:TimerX:3.1.0")
 
-    implementation("androidx.core:core-ktx:1.8.0")
+    implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.2")
-    implementation("androidx.activity:activity-compose:1.7.2")
+    implementation("androidx.activity:activity-compose:1.8.0")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation("androidx.navigation:navigation-compose:2.7.4")
     implementation("androidx.compose.ui:ui")
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
+    implementation("androidx.compose.material3:material3:1.1.2")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("androidx.constraintlayout:constraintlayout:2.1.4")
     testImplementation("junit:junit:4.13.2")
